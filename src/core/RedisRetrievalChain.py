9e3b235d9fbc71f26fc537cce18bc809021a8baf
Pieter Cailliau <pieter@redis.com>
2024-10-16 12:42:01 -0400

    docs

--- src/core/RedisRetrievalChain.py
+++ src/core/RedisRetrievalChain.py
@@ -79,20 +79,28 @@ class RedisRetrievalChain(Core):
 
 
     def __ask_question(self, q, callback_fn: StreamingStdOutCallbackHandlerYield):
-        # Chatbot with history managed by LangChain
+        # LLM Memory: Chatbot with history managed by LangChain
         redis_history = RedisChatMessageHistory(url=generate_redis_connection_string(REDIS_CFG["host"], REDIS_CFG["port"], REDIS_CFG["password"]),
                                                 session_id=self.session_id,
                                                 key_prefix='minipilot:history:',
                                                 ttl=MINIPILOT_HISTORY_TIMEOUT)
+        # LLM Memory: Limit the history length to MINIPILOT_HISTORY_LENGTH
+        history_length = redis_history.redis_client.llen(redis_history.key)
+        if (history_length > MINIPILOT_HISTORY_LENGTH):
+            redis_history.redis_client.rpop(redis_history.key, history_length - MINIPILOT_HISTORY_LENGTH)
 
-        # Managing the semantic cache
+        # LLM Memory: Helper method langchain
+        def get_chat_history(inputs) -> str:
+            return inputs
+
+        # Semantic Cache:
         if self.cfg.is_semantic_cache():
             cached = self.llmcache.check(prompt=q, return_fields=["response", "metadata"])
             if len(cached) > 0:
                 callback_fn.q.put(cached[0]['response'])
                 callback_fn.q.put(STOP_ITEM)
 
-                # The question is in the cache, but we want to save the conversation in the history too
+                # LLM Memory: The question is in the cache, but we want to save the conversation in the history too
                 metadata = {}
                 if 'metadata' in cached[0]:
                     metadata = cached[0]['metadata']
@@ -100,7 +108,14 @@ class RedisRetrievalChain(Core):
                 redis_history.add_message(BaseMessage(content=cached[0]['response'], type="ai", additional_kwargs=metadata))
                 return
 
-        # llm = OpenAI(streaming=True, callbacks=[callback_fn])
+        # Agentic memory: contstructing the QA prompts
+        messages = [
+            SystemMessagePromptTemplate.from_template(self.prompt_manager.get_system_prompt()['content']),
+            HumanMessagePromptTemplate.from_template(self.prompt_manager.get_user_prompt()['content'])
+        ]
+        qa_prompt = ChatPromptTemplate.from_messages(messages)
+
+        # Instantiating the LLM = OpenAI(streaming=True, callbacks=[callback_fn])
         streaming_llm = ChatOpenAI(
             model_name=self.model,
             streaming=True,
@@ -109,33 +124,19 @@ class RedisRetrievalChain(Core):
             callbacks=[callback_fn] #, StreamingStdOutCallbackHandler()
         )
 
-        # limit the history length to MINIPILOT_HISTORY_LENGTH
-        history_length = redis_history.redis_client.llen(redis_history.key)
-        if (history_length > MINIPILOT_HISTORY_LENGTH):
-            redis_history.redis_client.rpop(redis_history.key, history_length - MINIPILOT_HISTORY_LENGTH)
-
-        # Chatbot with custom history, here we choose a non-streaming LLM, or we will get the condensed question
-        # in the callback
-        def get_chat_history(inputs) -> str:
-            return inputs
-
-        messages = [
-            SystemMessagePromptTemplate.from_template(self.prompt_manager.get_system_prompt()['content']),
-            HumanMessagePromptTemplate.from_template(self.prompt_manager.get_user_prompt()['content'])
-        ]
-
-        qa_prompt = ChatPromptTemplate.from_messages(messages)
-
         """
         Chain for having a conversation based on retrieved documents.
         This chain takes in chat history (a list of messages) and new questions, and then returns an answer to that question. The algorithm for this chain consists of three parts:
-        1. Use the chat history and the new question to create a "standalone question". This is done so that this question can be passed into the retrieval step to fetch relevant documents. If only the new question was passed in, then relevant context may be lacking. If the whole conversation was passed into retrieval, there may be unnecessary information there that would distract from retrieval.
+        1. Use the chat history and the new question to create a "standalone question". This is done so that this question can be passed into the retrieval step to fetch relevant documents.
+           If only the new question was passed in, then relevant context may be lacking.
+           If the whole conversation was passed into retrieval, there may be unnecessary information there that would distract from retrieval.
         2. This new question is passed to the retriever and relevant documents are returned.
         3. The retrieved documents are passed to an LLM along with either the new question (default behavior) or the original question and chat history to generate a final response.
-        """
 
-        # now you can rebuild this using https://js.langchain.com/v0.1/docs/use_cases/code_understanding/#chat
-        # https://python.langchain.com/v0.2/docs/versions/migrating_chains/conversation_retrieval_chain/
+        note now you can rebuild this using https://js.langchain.com/v0.1/docs/use_cases/code_understanding/#chat
+        https://python.langchain.com/v0.2/docs/versions/migrating_chains/conversation_retrieval_chain/
+        """
+        # The actual chat bot construction:
         chatbot = ConversationalRetrievalChain.from_llm(llm=streaming_llm,
                                                         retriever=self.__get_retriever_with_score(MINIPILOT_CONTEXT_LENGTH),    # RAG
                                                         get_chat_history=get_chat_history,                                      # LLM Memory
@@ -154,25 +155,12 @@ class RedisRetrievalChain(Core):
             for doc in result['source_documents']:
                 references[doc.metadata['id'].split('idx:')[-1]] = doc.metadata
 
-            # decide if conversation history should be saved
+            # LLM memory: save history
             if self.cfg.is_memory():
                 redis_history.add_user_message(result["question"])
                 redis_history.add_message(BaseMessage(content=result["answer"], type="ai", additional_kwargs=references))
 
-            """
-            Update the cache only if caching is enabled AND there was context retrieved for RAG
-            First, we save the generated standalone question, which also includes summarization of the history
-            Second, having references means that there is semantic content available in the database, so the question should make sense
-            and it can be stored, otherwise the following might happen:
-            1. ask a question "can this or that happen?"
-            2. get references for RAG, forward the prompt to the LMM and get an answer, hence cache the pair question/asnwer
-            3. the user asks "are you completely sure?"
-            4. this interaction will cause the LLM to answer this last question based on conversation history
-            5. however, "are you completely sure?" will likely not produce relevant results for RAG (filtered out by a threshold)
-               and then it makes no sense to store the pair question/answer
-            """
-
-            # decide if the interaction should be cached
+            # Semantic Cache: decide if the interaction should be cached
             if len(references) and self.cfg.is_semantic_cache():
                 self.llmcache.store(prompt=result["generated_question"],
                                     response=result["answer"],
